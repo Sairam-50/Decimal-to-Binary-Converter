4809382e29bdedc45babd94ba31b6f3a04eb127d
Sairam-50 <sairams.shinrai74@gmail.com>
2024-01-17 09:45:28 +0530

    done

--- final_script.js
+++ final_script.js
@@ -3,45 +3,109 @@
 // What is the base case?
 // What is the least amount of work you need to do to get closer to the base case?
 
-
 // This function  will count down from a given number to zero using recursion.
-const countDownAndUp = (number) => {
-    console.log(number);
-    if(number === 0) {
-        console.log("Reached base case");
-        return;
-    }   else{
-countDownAndUp(number - 1);
-    }
-};
+// const countDownAndUp = (number) => {
+//     console.log(number);
+//     if(number === 0) {
+//         console.log("Reached base case");
+//         return;
+//     }   else{
+// countDownAndUp(number - 1);
+// console.log(number);
+//     }
+// };
 
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+const animationContainer = document.getElementById("animation-container");
+
+// this is the array that will hold the animation data. You'll use this array to show the animation when users try to convert the decimal number 5 to binary.
+const animationData = [
+  {
+    inputVal: 5,
+    marginTop: 300,
+    addElDelay: 1000,
+    msg: 'decimalToBinary(5) returns "10" + 1 (5 % 2). Then it pops off the stack.',
+    showMsgDelay: 15000,
+    removeElDelay: 20000,
+  },
+  {
+    inputVal: 2,
+    marginTop: -200,
+    addElDelay: 1500,
+    msg: 'decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.',
+    showMsgDelay: 10000,
+    removeElDelay: 15000,
+  },
+  {
+    inputVal: 1,
+    marginTop: -200,
+    addElDelay: 2000,
+    msg: 'decimalToBinary(1) returns "1" (base case) and gives that value to the stack below. Then it pops off the stack.',
+    showMsgDelay: 5000,
+    removeElDelay: 10000,
+  },
+];
+
+// You'll show the animation when users try to convert the decimal number 5 to binary, so you'll need to add a check for that within your checkUserInput() function.
+const showAnimation = () => {
+  result.innerText = "Call Stack Animation";
+
+  animationData.forEach((obj) => {
+    setTimeout(() => {
+      animationContainer.innerHTML += `
+        <p id="${obj.inputVal}" style="margin-top: ${obj.marginTop}px;" class="animation-frame">
+          decimalToBinary(${obj.inputVal})
+        </p>
+      `;
+    }, obj.addElDelay);
+
+    setTimeout(() => {
+      document.getElementById(obj.inputVal).textContent = obj.msg;
+    }, obj.showMsgDelay);
+
+    setTimeout(() => {
+      document.getElementById(obj.inputVal).remove();
+    }, obj.removeElDelay);
+  });
+
+  setTimeout(() => {
+    result.textContent = decimalToBinary(5);
+  }, 20000);
+};
 
 const decimalToBinary = (input) => {
-  let binary = "";
+  // if(input===0){
+  //   return "0";
+  // }else if(input===1){
+  //   return "1";
+  // }
 
-  if (input === 0) {
-    binary = "0";
+  // A better way:
+
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
-
-  while (input > 0) {
-    binary = (input % 2) + binary;
-    input = Math.floor(input / 2);
-  }
-
-  result.innerText = binary;
 };
 
 const checkUserInput = () => {
-  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+  const inputInt = parseInt(numberInput.value);
+  if (!numberInput.value || isNaN(inputInt)) {
     alert("Please provide a decimal number");
     return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
+  if (inputInt === 5) {
+    showAnimation();
+    return;
+  }
+
+  result.textContent = decimalToBinary(inputInt);
   numberInput.value = "";
+  // the above line is to clear the input field after the conversion is done. if this is not done, the input field will still have the value that was entered by the user.
 };
 
 convertBtn.addEventListener("click", checkUserInput);
